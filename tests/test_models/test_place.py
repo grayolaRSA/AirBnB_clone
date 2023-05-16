7792d468b7f5ae324567e3f92646330b9e0bb4de
grayolaRSA <gtndanga@gmail.com>
2023-05-16 17:18:57 +0200

    update

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,33 +7,103 @@ from models.base_model import BaseModel
 
 class TestPlace(unittest.TestCase):
     """class to test place class in console"""
-    def test_attributes(self):
-        """Test that the Place class has the correct attributes"""
-        place = Place()
-        self.assertTrue(hasattr(place, "city_id"))
-        self.assertEqual(place.city_id, "")
-        self.assertTrue(hasattr(place, "user_id"))
-        self.assertEqual(place.user_id, "")
-        self.assertTrue(hasattr(place, "name"))
-        self.assertEqual(place.name, "")
-        self.assertTrue(hasattr(place, "description"))
-        self.assertEqual(place.description, "")
-        self.assertTrue(hasattr(place, "number_rooms"))
-        self.assertEqual(place.number_rooms, 0)
-        self.assertTrue(hasattr(place, "number_bathrooms"))
-        self.assertEqual(place.number_bathrooms, 0)
-        self.assertTrue(hasattr(place, "max_guest"))
-        self.assertEqual(place.max_guest, 0)
-        self.assertTrue(hasattr(place, "price_by_night"))
-        self.assertEqual(place.price_by_night, 0)
-        self.assertTrue(hasattr(place, "latitude"))
-        self.assertEqual(place.latitude, 0.0)
-        self.assertTrue(hasattr(place, "longitude"))
-        self.assertEqual(place.longitude, 0.0)
-        self.assertTrue(hasattr(place, "amenity_ids"))
-        self.assertEqual(place.amenity_ids, [])
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.place1 = Place()
+        cls.place1.name = "Nairobi"
+
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.place1
+
+    def test_class_exists(self):
+        """Tests if class exists.
+        """
+        result = "<class 'models.place.Place'>"
+        self.assertEqual(str(type(self.place1)), result)
 
     def test_inheritance(self):
-        """Test that the Place class inherits from BaseModel"""
-        place = Place()
-        self.assertIsInstance(place, BaseModel)
+        """Test if Place is a subclass and instace of BaseModel.
+        """
+        self.assertIsInstance(self.place1, Place)
+        self.assertEqual(type(self.place1), Place)
+        self.assertEqual(issubclass(self.place1.__class__, BaseModel), True)
+
+    def test_types(self):
+        """Test if attributes type is correct.
+        """
+        self.assertIsInstance(self.place1.name, str)
+        self.assertEqual(type(self.place1.name), str)
+        self.assertIsInstance(self.place1.id, str)
+        self.assertEqual(type(self.place1.id), str)
+        self.assertIsInstance(self.place1.created_at, datetime.datetime)
+        self.assertIsInstance(self.place1.updated_at, datetime.datetime)
+        self.assertIsInstance(self.place1.amenity_ids, list)
+        self.assertIsInstance(self.place1.longitude, float)
+        self.assertIsInstance(self.place1.latitude, float)
+        self.assertIsInstance(self.place1.price_by_night, int)
+        self.assertIsInstance(self.place1.max_guest, int)
+        self.assertIsInstance(self.place1.description, str)
+        self.assertIsInstance(self.place1.number_rooms, int)
+        self.assertIsInstance(self.place1.number_bathrooms, int)
+        self.assertIsInstance(self.place1.city_id, str)
+        self.assertIsInstance(self.place1.user_id, str)
+
+    def test_save(self):
+        """Test if save method is working correctly after update.
+        """
+        self.place1.save()
+        self.assertNotEqual(self.place1.created_at, self.place1.updated_at)
+
+    def test_functions(self):
+        """Test if Place module is documented.
+        """
+        self.assertIsNotNone(Place.__doc__)
+
+    def test_has_attributes(self):
+        """Test if expected attributes exist.
+        """
+        self.assertTrue(hasattr(self.place1, 'name'))
+        self.assertTrue(hasattr(self.place1, 'id'))
+        self.assertTrue(hasattr(self.place1, 'created_at'))
+        self.assertTrue(hasattr(self.place1, 'updated_at'))
+        self.assertTrue(hasattr(self.place1, 'city_id'))
+        self.assertTrue(hasattr(self.place1, 'user_id'))
+        self.assertTrue(hasattr(self.place1, 'description'))
+        self.assertTrue(hasattr(self.place1, 'number_rooms'))
+        self.assertTrue(hasattr(self.place1, 'number_bathrooms'))
+        self.assertTrue(hasattr(self.place1, 'max_guest'))
+        self.assertTrue(hasattr(self.place1, 'price_by_night'))
+        self.assertTrue(hasattr(self.place1, 'latitude'))
+        self.assertTrue(hasattr(self.place1, 'longitude'))
+        self.assertTrue(hasattr(self.place1, 'amenity_ids'))
+
+    def test_to_dict(self):
+        """Test if to_dict method is working correctly.
+        """
+        my_model_json = self.place1.to_dict()
+        self.assertEqual(str, type(my_model_json['created_at']))
+        self.assertEqual(my_model_json['created_at'],
+                         self.place1.created_at.isoformat())
+        self.assertEqual(datetime.datetime, type(self.place1.created_at))
+        self.assertEqual(my_model_json['__class__'],
+                         self.place1.__class__.__name__)
+        self.assertEqual(my_model_json['id'], self.place1.id)
+
+    def test_unique_id(self):
+        """Test if each instance is created with a unique ID.
+        """
+        place2 = self.place1.__class__()
+        place3 = self.place1.__class__()
+        place4 = self.place1.__class__()
+        self.assertNotEqual(self.place1.id, place2.id)
+        self.assertNotEqual(self.place1.id, place3.id)
+        self.assertNotEqual(self.place1.id, place4.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -7,36 +7,89 @@ from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-    def test_attributes(self):
-        user = User()
-        self.assertTrue(hasattr(user, "email"))
-        self.assertTrue(hasattr(user, "password"))
-        self.assertTrue(hasattr(user, "first_name"))
-        self.assertTrue(hasattr(user, "last_name"))
-        self.assertEqual(user.email, "")
-        self.assertEqual(user.password, "")
-        self.assertEqual(user.first_name, "")
-        self.assertEqual(user.last_name, "")
+    """Defines tests for User Class"""
+
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.user1 = User()
+        cls.user1.name = "Wendy"
+
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.user1
+
+    def test_class_exists(self):
+        """Tests if class exists.
+        """
+        result = "<class 'models.user.User'>"
+        self.assertEqual(str(type(self.user1)), result)
 
     def test_inheritance(self):
-        user = User()
-        self.assertIsInstance(user, BaseModel)
+        """Test if User is a subclass and instace of BaseModel.
+        """
+        self.assertIsInstance(self.user1, User)
+        self.assertEqual(type(self.user1), User)
+        self.assertEqual(issubclass(self.user1.__class__, BaseModel), True)
 
-    def test_storage(self):
-        user = User()
-        user.email = "test@test.com"
-        user.password = "password"
-        user.first_name = "John"
-        user.last_name = "Doe"
-        storage.new(user)
-        key = "User." + user.id
-        obj = storage.all().get(key)
-        self.assertIsNotNone(obj)
-        self.assertIsInstance(obj, User)
-        self.assertEqual(obj.email, "test@test.com")
-        self.assertEqual(obj.password, "password")
-        self.assertEqual(obj.first_name, "John")
-        self.assertEqual(obj.last_name, "Doe")
+    def test_types(self):
+        """Test if attributes type is correct.
+        """
+        self.assertIsInstance(self.user1.id, str)
+        self.assertEqual(type(self.user1.id), str)
+        self.assertIsInstance(self.user1.created_at, datetime.datetime)
+        self.assertIsInstance(self.user1.updated_at, datetime.datetime)
+        self.assertIsInstance(self.user1.first_name, str)
+        self.assertIsInstance(self.user1.last_name, str)
+        self.assertIsInstance(self.user1.email, str)
+        self.assertIsInstance(self.user1.password, str)
+
+    def test_save(self):
+        """Test if save method is working correctly after update.
+        """
+        self.user1.save()
+        self.assertNotEqual(self.user1.created_at, self.user1.updated_at)
+
+    def test_functions(self):
+        """Test if User module is documented.
+        """
+        self.assertIsNotNone(User.__doc__)
+
+    def test_has_attributes(self):
+        """Test if expected attributes exist.
+        """
+        self.assertTrue(hasattr(self.user1, 'id'))
+        self.assertTrue(hasattr(self.user1, 'created_at'))
+        self.assertTrue(hasattr(self.user1, 'updated_at'))
+        self.assertTrue(hasattr(self.user1, 'first_name'))
+        self.assertTrue(hasattr(self.user1, 'last_name'))
+        self.assertTrue(hasattr(self.user1, 'email'))
+        self.assertTrue(hasattr(self.user1, 'password'))
+
+    def test_to_dict(self):
+        """Test if to_dict method is working correctly.
+        """
+        my_model_json = self.user1.to_dict()
+        self.assertEqual(str, type(my_model_json['created_at']))
+        self.assertEqual(my_model_json['created_at'],
+                         self.user1.created_at.isoformat())
+        self.assertEqual(datetime.datetime, type(self.user1.created_at))
+        self.assertEqual(my_model_json['__class__'],
+                         self.user1.__class__.__name__)
+        self.assertEqual(my_model_json['id'], self.user1.id)
+
+    def test_unique_id(self):
+        """Test if each instance is created with a unique ID.
+        """
+        user2 = self.user1.__class__()
+        user3 = self.user1.__class__()
+        user4 = self.user1.__class__()
+        self.assertNotEqual(self.user1.id, user2.id)
+        self.assertNotEqual(self.user1.id, user3.id)
+        self.assertNotEqual(self.user1.id, user4.id)
 
 
 if __name__ == '__main__':
