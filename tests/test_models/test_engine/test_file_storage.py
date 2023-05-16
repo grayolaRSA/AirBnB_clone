3014179e3115ccd9447d166b1b1b748f98bb4b7e
grayolaRSA <gtndanga@gmail.com>
2023-05-16 17:30:21 +0200

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,54 +10,55 @@ from models.engine.file_storage import FileStorage
 class TestFileStorage(unittest.TestCase):
     """Test cases for FileStorage class"""
 
-    def setUp(self):
-        """Set up test environment"""
-        self.file_path = "file.json"
-        self.storage = FileStorage()
-        self.bm = BaseModel()
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.base_model1 = BaseModel()
+        cls.file_storage1 = FileStorage()
 
-        if not os.path.exists(self.file_path):
-            with open(self.file_path, "w") as f:
-                f.write("{}")
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.base_model1
+        del cls.file_storage1
 
-    def test_all(self):
-        """Test for all method"""
-        self.assertEqual(type(self.storage.all()), dict)
-        self.assertIs(self.storage.all(), self.storage._FileStorage__objects)
+    def test_class_exists(self):
+        """Tests if class exists.
+        """
+        result = "<class 'models.engine.file_storage.FileStorage'>"
+        self.assertEqual(str(type(self.file_storage1)), result)
 
-    def test_new(self):
-        """Test for new method"""
-        key = self.bm.__class__.__name__ + "." + self.bm.id
-        self.storage.new(self.bm)
-        self.assertIn(key, self.storage._FileStorage__objects.keys())
-        self.assertIn(self.bm, self.storage._FileStorage__objects.values())
+    def test_types(self):
+        """Test if attributes type is correct.
+        """
+        self.assertIsInstance(self.file_storage1, FileStorage)
+        self.assertEqual(type(self.file_storage1), FileStorage)
+
+    def test_functions(self):
+        """Test if FileStorage module is documented.
+        """
+        self.assertIsNotNone(FileStorage.__doc__)
 
     def test_save(self):
-        """Test for save method"""
-        bm = BaseModel()
-        self.storage.new(bm)
-        self.storage.save()
-        with open(self.file_path, "r") as f:
-            objs = json.load(f)
-        obj_key = "{}.{}".format(type(bm).__name__, bm.id)
-        self.assertIn(obj_key, objs.keys())
+        """Test if save method is working correctly.
+        """
+        self.file_storage1.save()
+        self.assertEqual(os.path.exists(storage._FileStorage__file_path), True)
+        self.assertEqual(storage.all(), storage._FileStorage__objects)
 
     def test_reload(self):
-        """Test for reload method"""
-        bm = BaseModel()
-        self.storage.new(bm)
-        self.storage.save()
-        self.storage.reload()
-        objs = self.storage.all()
-        obj_key = "{}.{}".format(type(bm).__name__, bm.id)
-        self.assertIn(obj_key, objs.keys())
-
-    def tearDown(self):
-        """Remove file.json created"""
-        try:
-            os.remove(self.file_path)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        """Tests if reload method is working correctly.
+        """
+        self.base_model1.save()
+        self.assertEqual(os.path.exists(storage._FileStorage__file_path), True)
+        dobj = storage.all()
+        FileStorage._FileStorage__objects = {}
+        self.assertNotEqual(dobj, FileStorage._FileStorage__objects)
+        storage.reload()
+        for key, value in storage.all().items():
+            self.assertEqual(dobj[key].to_dict(), value.to_dict())
 
 
 if __name__ == '__main__':
