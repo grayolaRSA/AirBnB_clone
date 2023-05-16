389df5c7cd634ddb72cef907f4ade025202bf199
grayolaRSA <gtndanga@gmail.com>
2023-05-16 18:55:44 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,7 @@ class BaseModel:
     def save(self):
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
