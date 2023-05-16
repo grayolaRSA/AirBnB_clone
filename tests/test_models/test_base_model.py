e085502b18e5407a439d6491f6f2b84501413dd2
grayolaRSA <gtndanga@gmail.com>
2023-05-16 18:09:35 +0200

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,62 +8,28 @@ import datetime
 class TestBaseModel(unittest.TestCase):
     """Test cases for BaseModel"""
 
-    def setUp(self):
-        """create instance"""
-        self.base_model = BaseModel()
+    @classmethod
+    def setUp(cls):
+        """Runs for each test case.
+        """
+        cls.BaseModel1 = BaseModel()
+        cls.BaseModel1.name = "Samsung"
+        cls.BaseModel1.my_number = 89
 
-    def tearDown(self):
-        """Delete instance"""
-        del self.base_model
-
-    def test_instance_attributes(self):
-        """Test instance attributes"""
-        self.assertTrue(hasattr(self.base_model, "id"))
-        self.assertTrue(hasattr(self.base_model, "created_at"))
-        self.assertTrue(hasattr(self.base_model, "updated_at"))
-        self.assertEqual(self.base_model.__class__.__name__, "BaseModel")
-
-    def test_id_type(self):
-        """Test if id is of type string"""
-        self.assertEqual(type(self.base_model.id), str)
-
-    def test_created_at_type(self):
-        """Test if created_at is of type datetime"""
-        self.assertEqual(type(self.base_model.created_at), datetime.datetime)
-
-    def test_updated_at_type(self):
-        """Test if updated_at is of type datetime"""
-        self.assertEqual(type(self.base_model.updated_at), datetime.datetime)
-
-    def test_to_dict(self):
-        """Test to_dict method"""
-        dict_ = self.base_model.to_dict()
-        self.assertEqual(dict_["__class__"], "BaseModel")
-        self.assertEqual(type(dict_["created_at"]), str)
-        self.assertEqual(type(dict_["updated_at"]), str)
-
-    def test_str_method(self):
-        """Test __str__ method"""
-        string = str(self.base_model)
-        self.assertEqual("[BaseModel] ({}) {}".format(
-            self.base_model.id, self.base_model.__dict__), string)
-
-    def test_save_method(self):
-        """Test save method"""
-        self.base_model.save()
-        self.assertNotEqual(self.base_model.created_at,
-                            self.base_model.updated_at)
+    @classmethod
+    def tearDown(cls):
+        """Cleans up after each test.
+        """
+        del cls.BaseModel1
 
     def test_class_exists(self):
-        """
-        Tests if class exists.
+        """Tests if class exists.
         """
         result = "<class 'models.base_model.BaseModel'>"
         self.assertEqual(str(type(self.BaseModel1)), result)
 
     def testBaseModel1(self):
-        """
-        Test attributes value of a BaseModel instance.
+        """Test attributes value of a BaseModel instance.
         """
         self.BaseModel1.save()
         my_model_json = self.BaseModel1.to_dict()
@@ -74,8 +40,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(self.BaseModel1.id, my_model_json['id'])
 
     def test_types(self):
-        """
-        Test if attributes type is correct.
+        """Test if attributes type is correct.
         """
         self.assertIsInstance(self.BaseModel1.name, str)
         self.assertEqual(type(self.BaseModel1.name), str)
@@ -84,15 +49,20 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(self.BaseModel1.created_at, datetime.datetime)
         self.assertIsInstance(self.BaseModel1.updated_at, datetime.datetime)
 
-    def test_functions(self):
+    def test_save(self):
+        """Test if save method is working correctly after update.
         """
-        Test if BaseModel moudule is documented.
+        self.BaseModel1.save()
+        self.assertNotEqual(self.BaseModel1.created_at,
+                            self.BaseModel1.updated_at)
+
+    def test_functions(self):
+        """Test if BaseModel moudule is documented.
         """
         self.assertIsNotNone(BaseModel.__doc__)
 
     def test_has_attributes(self):
-        """
-        Test if expected attributes exist.
+        """Test if expected attributes exist.
         """
         self.assertTrue(hasattr(self.BaseModel1, 'name'))
         self.assertTrue(hasattr(self.BaseModel1, 'id'))
@@ -100,15 +70,13 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.BaseModel1, 'updated_at'))
 
     def test_set_attributes(self):
-        """
-        Test set attributes of BaseModel.
+        """Test set attributes of BaseModel.
         """
         self.assertEqual(self.BaseModel1.name, "Samsung")
         self.assertEqual(self.BaseModel1.my_number, 89)
 
     def test_to_dict(self):
-        """
-        Test if to_dict method is working correctly.
+        """Test if to_dict method is working correctly.
         """
         my_model_json = self.BaseModel1.to_dict()
         self.assertEqual(str, type(my_model_json['created_at']))
@@ -120,8 +88,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(my_model_json['id'], self.BaseModel1.id)
 
     def test_unique_id(self):
-        """
-        Test if each instance is created with a unique ID.
+        """Test if each instance is created with a unique ID.
         """
         basemodel2 = self.BaseModel1.__class__()
         basemodel3 = self.BaseModel1.__class__()
@@ -131,8 +98,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertNotEqual(self.BaseModel1.id, basemodel4.id)
 
     def test__str__(self):
-        """
-        Test if __str__ method returns expected string.
+        """Test if __str__ method returns expected string.
         """
         string = str(self.BaseModel1)
         id_test = "[BaseModel] ({})".format(self.BaseModel1.id)
